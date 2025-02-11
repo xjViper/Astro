9ee4191267e79fedd45bc3b26a5451a17db7682a
João Vitor Perfeito <jvperfeito@gmail.com>
2025-02-11 17:30:02 -0300

    ✨ feat: improve layout for filters and add filters on professor page

--- src/layouts/helpers/Filters.tsx
+++ src/layouts/helpers/Filters.tsx
@@ -1,44 +1,96 @@
 import { filters } from "@/stores/filters";
 import { useStore } from "@nanostores/react";
-import React from "react";
+import {
+  FaSortAmountDownAlt,
+  FaSortAmountDown,
+  FaFilter,
+} from "react-icons/fa";
+import React, { useState } from "react";
 
 const Filters = () => {
   const $filterData = useStore(filters);
+
+  const [orderDirection, setOrderDirection] = useState<"asc" | "desc">("asc");
 
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     filters.setKey("searchText", e.target.value);
   };
 
-  const handleOrderChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleOrderChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     filters.setKey("orderBy", e.target.value as "name" | "level" | "rank");
   };
 
-  const handleDirectionChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    filters.setKey("orderDirection", e.target.value as "asc" | "desc");
+  const handleDirectionChange = () => {
+    const newDirection = orderDirection === "asc" ? "desc" : "asc";
+    setOrderDirection(newDirection);
+    filters.setKey("orderDirection", newDirection);
   };
 
   return (
-    <div>
+    <div className="flex flex-row justify-around items-center">
+      <h4 className="flex flex-row gap-1">
+        <FaFilter /> Filtros:
+      </h4>
       <input
         type="text"
+        className="sm:w-100 form-input md:w-1/4 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none border-"
         placeholder="Buscar"
         value={$filterData.searchText}
         onChange={handleSearchChange}
       />
 
-      <select value={$filterData.orderBy} onChange={handleOrderChange}>
-        <option value="name">Nome</option>
-        <option value="level">Level</option>
-        <option value="rank">Rank</option>
-      </select>
+      <div className="flex md:w-1/4 justify-around">
+        <div className="flex items-center gap-1">
+          <input
+            type="radio"
+            id="name"
+            name="orderBy"
+            value="name"
+            checked={$filterData.orderBy === "name"}
+            onChange={handleOrderChange}
+          />
+          <label htmlFor="name">Nome</label>
+        </div>
 
-      <select
-        value={$filterData.orderDirection}
-        onChange={handleDirectionChange}
+        <div className="flex items-center gap-1">
+          <input
+            type="radio"
+            id="level"
+            name="orderBy"
+            value="level"
+            checked={$filterData.orderBy === "level"}
+            onChange={handleOrderChange}
+          />
+          <label htmlFor="level">Level</label>
+        </div>
+
+        <div className="flex items-center gap-1">
+          <input
+            type="radio"
+            id="rank"
+            name="orderBy"
+            value="rank"
+            checked={$filterData.orderBy === "rank"}
+            onChange={handleOrderChange}
+          />
+          <label htmlFor="rank">Rank</label>
+        </div>
+      </div>
+
+      <button
+        onClick={handleDirectionChange}
+        className="flex gap-1 items-center w-[10%]"
       >
-        <option value="asc">Crescente</option>
-        <option value="desc">Decrescente</option>
-      </select>
+        {orderDirection === "asc" ? (
+          <>
+            <FaSortAmountDownAlt /> Crescente
+          </>
+        ) : (
+          <>
+            <FaSortAmountDown /> Decrescente
+          </>
+        )}
+      </button>
     </div>
   );
 };
